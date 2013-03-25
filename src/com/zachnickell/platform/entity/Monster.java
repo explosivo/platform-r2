f475de2da645f5f0b46420776784f0f7d722219f
Zach Nickell <zach.x.nickell@gmail.com>
2013-03-24 22:27:24 -0500

    push fix
    some files that should have been committed weren't. stupid eclipse.

---
+++ src/com/zachnickell/platform/entity/Monster.java
@@ -0,0 +1,8 @@
+package com.zachnickell.platform.entity;
+
+public class Monster extends Entity {
+
+	public Monster() {
+	}
+
+}
---
+++ src/com/zachnickell/platform/gfx/Sprites.java
@@ -0,0 +1,100 @@
+package com.zachnickell.platform.gfx;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.zachnickell.platform.entity.tile.Tile;
+
+public class Sprites {
+	static int size = Tile.size;
+
+	public static BufferedImage defaultSprite = new BufferedImage(size, size,
+			BufferedImage.TYPE_INT_RGB);
+	public static BufferedImage snow = new BufferedImage(size, size,
+			BufferedImage.TYPE_INT_RGB);
+	public static BufferedImage dirt = new BufferedImage(size, size,
+			BufferedImage.TYPE_INT_RGB);
+	public static BufferedImage spaceship = new BufferedImage(size, size,
+			BufferedImage.TYPE_INT_RGB);
+	public static BufferedImage heart = new BufferedImage(size, size,
+			BufferedImage.TRANSLUCENT);
+
+	public Sprites() {
+		makeDefaultSprite();
+		makeSnowSprite();
+		makeDirtSprite();
+		makeHeartSprite();
+	}
+
+	public BufferedImage getImage(String s) {
+		BufferedImage i = null;
+		try {
+			i = ImageIO.read(Sprites.class.getResource(s));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+
+	public void makeDefaultSprite() {
+		Graphics g = defaultSprite.createGraphics();
+		g.setColor(Color.white);
+		g.fillRect(0 * 8, 0 * 8, 8, 8);
+		g.fillRect(0 * 8 + 8, 0 * 8 + 8, 8, 8);
+		g.setColor(Color.black);
+		g.fillRect(0 * 8 + 8, 0 * 8, 8, 8);
+		g.fillRect(0 * 8, 0 * 8 + 8, 8, 8);
+		g.dispose();
+	}
+
+	public void makeSnowSprite() {
+		snow = getImage("/snow.png");
+		if (snow == null) {
+			Graphics g = snow.createGraphics();
+			g.setColor(Color.GRAY);
+			g.fillRect(0 * 8, 0 * 8, 8, 8);
+			g.fillRect(0 * 8 + 8, 0 * 8 + 8, 8, 8);
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0 * 8 + 8, 0 * 8, 8, 8);
+			g.fillRect(0 * 8, 0 * 8 + 8, 8, 8);
+			g.dispose();
+		}
+	}
+
+	public void makeDirtSprite() {
+		dirt = getImage("/dirt.png");
+		if (dirt == null) {
+			Graphics g = dirt.createGraphics();
+			g.setColor(Color.ORANGE);
+			g.fillRect(0 * 8, 0 * 8, 8, 8);
+			g.fillRect(0 * 8 + 8, 0 * 8 + 8, 8, 8);
+			g.setColor(Color.BLUE);
+			g.fillRect(0 * 8 + 8, 0 * 8, 8, 8);
+			g.fillRect(0 * 8, 0 * 8 + 8, 8, 8);
+			g.dispose();
+		}
+	}
+
+	public void makeHeartSprite() {
+		if (heart == null) {
+			Graphics g = heart.createGraphics();
+			g.setColor(Color.RED);
+			g.fillOval(0, 0, size, size);
+			g.dispose();
+		}
+	}
+
+	public void makeSpaceShipSprite() {
+		if (spaceship == null) {
+			Graphics g = spaceship.createGraphics();
+			g.setColor(Color.GRAY);
+			g.fillRect(0, 0, size, size);
+			g.dispose();
+		}
+	}
+
+}
